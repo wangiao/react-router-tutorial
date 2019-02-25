dcb06c54d5f6390d1421b5e0e62cfdad101e5d49
wangmiao <wangiao@foxmail.com>
2019-02-25 22:53:27 +0800

    lesson5 添加注释笔记

--- lessons/05-active-links/modules/NavLink.js
+++ lessons/05-active-links/modules/NavLink.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+//NavLink的作用在于可以统一配置Link组件的属性，起到了分类的作用
 export default React.createClass({
     render() {
         return <Link {...this.props} activeClassName="active"/>
