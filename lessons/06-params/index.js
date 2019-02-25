c2bb1d6c6ec2cd955341069e420df29655dd4dd4
wangmiao <wangiao@foxmail.com>
2019-02-25 23:54:23 +0800

    lesson6 done

--- lessons/06-params/index.js
+++ lessons/06-params/index.js
@@ -4,12 +4,15 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="/repos" component={Repos}/>
-      <Route path="/about" component={About}/>
-    </Route>
-  </Router>
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <Route path="/repos" component={Repos}/>
+            {/* add the new route */}
+            <Route path="/repos/:userName/:repoName" component={Repo}/>
+            <Route path="/about" component={About}/>
+        </Route>
+    </Router>
 ), document.getElementById('app'))
