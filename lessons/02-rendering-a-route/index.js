c2ec605c827893e47cb8de4d2e7bada399671850
wangmiao <wangiao@foxmail.com>
2019-02-25 20:16:05 +0800

    lesson2 done

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -1,4 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import Repos from "../03-navigating-with-link/modules/Repos";
+import About from "../03-navigating-with-link/modules/About";
+
+render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}/>
+        <Route path="/repos" component={Repos}/>
+        <Route path="/about" component={About}/>
+    </Router>
+), document.getElementById('app'))
