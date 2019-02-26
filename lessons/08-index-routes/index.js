58f4f176459cb65b2a8dc1c639dea4ef7a5064db
wangmiao <wangiao@foxmail.com>
2019-02-27 04:33:57 +0800

    lesson8 done

--- lessons/08-index-routes/index.js
+++ lessons/08-index-routes/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
 import Repo from './modules/Repo'
+import Home from './modules/Home'
 
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="/repos" component={Repos}>
-        <Route path="/repos/:userName/:repoName" component={Repo}/>
-      </Route>
-      <Route path="/about" component={About}/>
-    </Route>
-  </Router>
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+
+            {/* add it here, as a child of `/` */}
+            {/*只有新打开页面的时候，才会加载home组件。已经做过点选Link的动作后不会再显示Home*/}
+            <IndexRoute component={Home}/>
+
+            <Route path="/repos" component={Repos}>
+                <Route path="/repos/:userName/:repoName" component={Repo}/>
+            </Route>
+            <Route path="/about" component={About}/>
+        </Route>
+    </Router>
 ), document.getElementById('app'))
